9e16f35201aa4cc5b0aff2c6cf05291caaae6612
JU MIN JONG <wnalsals12@naver.com>
2022-10-18 02:51:57 +0900

    백그라운드 색상변경

--- dajindan/src/App.vue
+++ dajindan/src/App.vue
@@ -24,7 +24,7 @@ body {margin: 0;}
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  background-color: #2c3e50;
+  background-color: #031828;
   width: 100vw;
   height: 100vh;
   display: flex;
